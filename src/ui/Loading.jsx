784fea6eaebe56878aa5b214e40c0a89364a8b46
akamensi <akez.muhammed@gmail.com>
2024-07-31 20:30:23 +0200

    Absolute Position: z-index and more

--- src/ui/Loading.jsx
+++ src/ui/Loading.jsx
@@ -1,5 +1,9 @@
 function Loading() {
-  return <div className="loader"></div>;
+  return (
+    <div className="gb-slate-200/20 absolute inset-0 flex items-center justify-center backdrop-blur-sm">
+      <div className="loader"></div>
+    </div>
+  );
 }
 
 export default Loading;
