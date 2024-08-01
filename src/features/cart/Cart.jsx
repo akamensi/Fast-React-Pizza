f90802be174748391cf838369e189bbeaacf273a
akamensi <akez.muhammed@gmail.com>
2024-08-01 19:30:29 +0200

    Updating Cart Quantity

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -49,7 +49,7 @@ function Cart() {
       <h2 className="mt-7 text-xl font-semibold">Your cart, {username} </h2>
       <ul className="divide-y divide-stone-200 border-b">
         {cart.map((item) => (
-          <CartItem item={item} key={item.id} />
+          <CartItem item={item} key={item.pizzaId} />
         ))}
       </ul>
 
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,9 @@
-import { useDispatch, useSelector } from 'react-redux';
-import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
+import { useDispatch, useSelector } from 'react-redux';
 import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
+import Button from '../../ui/Button';
 import DeleteItem from '../cart/DeleteItem';
+import UpdateItemQuantity from '../cart/UpdateItemQuantity';
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
@@ -43,7 +44,15 @@ function MenuItem({ pizza }) {
           ) : (
             <p>Sold out</p>
           )}
-          {isInCart && <DeleteItem pizzaId={id} />}
+          {isInCart && (
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
 
           {!soldOut && !isInCart && (
             <Button onClick={handleAddToCart} type="small">
