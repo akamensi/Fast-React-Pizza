28f491e7f54db9df671364b1ac30f63541463dcf
akamensi <akez.muhammed@gmail.com>
2024-08-02 01:56:03 +0200

    Updating Data Using useFetcher and <fetcher.form>

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./ui/Home";
-import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './ui/Home';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
 import CreateOrder, {
   action as createOrderAction,
-} from "./features/order/CreateOrder";
-import Order, { loader as orderLoader } from "./features/order/Order";
-import AppLayout from "./ui/AppLayout";
-import Error from "./ui/Error";
+} from './features/order/CreateOrder';
+import { action as updateOrderAction } from './features/order/UpdateOrder';
+import Order, { loader as orderLoader } from './features/order/Order';
+import AppLayout from './ui/AppLayout';
+import Error from './ui/Error';
 
 //I use createBrowserRouter in order to enable data fetching or data loading or to sumbit data using forms with React Router
 const router = createBrowserRouter([
@@ -15,25 +16,26 @@ const router = createBrowserRouter([
     element: <AppLayout /> /* AppLayout is a Layout Route */,
     errorElement: <Error />,
     children: [
-      { path: "/", element: <Home /> },
+      { path: '/', element: <Home /> },
       {
-        path: "/menu",
+        path: '/menu',
         element: <Menu />,
         loader:
           menuLoader /*provide the menu data using the menuLoader to the menu page */,
         errorElement: <Error />,
       },
-      { path: "/cart", element: <Cart /> },
+      { path: '/cart', element: <Cart /> },
       {
-        path: "/order/new",
+        path: '/order/new',
         element: <CreateOrder />,
         action: createOrderAction,
       },
       {
-        path: "/order/:orderId",
+        path: '/order/:orderId',
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
